d615e0a3887e160d7c1df90e7cb5eb2b7d16a6ee
Isamar carolina Valdez <70655953230@info.local>
2023-11-08 21:57:18 -0300

    API

---
+++ src/main/java/com/senai/apivsconnect/dtos/TokenDto.java
@@ -0,0 +1,8 @@
+package com.senai.apivsconnect.dtos;
+
+public record TokenDto(String token) {
+
+
+
+
+}
--- src/main/java/com/senai/apivsconnect/services/TokenService.java
+++ src/main/java/com/senai/apivsconnect/services/TokenService.java
@@ -1,8 +1,15 @@
 package com.senai.apivsconnect.services;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.senai.apivsconnect.models.UsuarioModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -11,5 +18,41 @@ public class TokenService {
 
     private final Algorithm algoritmo = Algorithm.HMAC256(secret);
 
+public String gerarToken(UsuarioModel usuario){
+  try  {
+      Algorithm algoritmo = Algorithm.HMAC256(secret);
+      String token = JWT.create()
+              .withIssuer("api-vsconnect")
+              .withSubject(usuario.getEmail())
+              .withClaim("nomeUsuario", usuario.getNome())
+              .withExpiresAt(gerarValidadeToken())
+              .sign(algoritmo);
 
+              return token;
+
+
+  }catch(JWTCreationException exception){
+      throw new RuntimeException("erro na criacao do token: ", exception);
+
+  }
+}
+
+public  String validarToken(String token){
+ try {
+     Algorithm algoritmo = Algorithm.HMAC256(secret);
+
+     return JWT.require(algoritmo)
+             .withIssuer("api-vsconnect")
+             .build()
+             .verify(token)
+             .getSubject();
+
+ } catch(JWTCreationException exception) {
+     return "";
+ }
+
+}
+public Instant gerarValidadeToken() {
+    return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+}
 }
